9b0ae291cd952bac4ef47610691d94ee428714b5
ScreenBottle <arvid.rydh@iths.se>
2023-03-21 08:41:21 +0100

    first files

---
+++ artsearch.js
@@ -0,0 +1,6 @@
+const departmentsUrl = 'https://collectionapi.metmuseum.org/public/collection/v1/departments'
+
+const getDepartmentList = async () => {
+    const response = await fetch(departmentsUrl);
+    
+}
